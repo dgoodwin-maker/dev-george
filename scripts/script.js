68d9b49d25e6de774639285fecc35742e1fbf208
dgoodwin-maker <dgoodwin3@una.edu>
2025-04-28 22:43:07 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,36 @@
 $(document).ready(function () {
+  [
+  {
+    "id": 1,
+    "title": "Complete Dev-George Project",
+    "description": "Finish this final web development project for the semester",
+    "dueDate": "2025-04-28",
+    "priority": "High",
+    "completed": false
+  },
+  {
+    "id": 2,
+    "title": "Study for Web Dev Exam",
+    "description": "Review JavaScript and pray a little harder as well",
+    "dueDate": "TBD",
+    "priority": "Medium",
+    "completed": false
+  },
+  {
+    "id": 3,
+    "title": "Read more",
+    "description": "Read brotha man",
+    "dueDate": "2025-05-10",
+    "priority": "Low",
+    "completed": false
+  },
+  {
+    "id": 4,
+    "title": "Learn GitHub Basics",
+    "description": "Ability to learn GitHub basics",
+    "dueDate": "2025-04-29",
+    "priority": "Medium",
+    "completed": true
+  }
+]
 }
